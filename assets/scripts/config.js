e32d462d09025c48003dd3f0a7b1b99ab76996b9
Tran Ngoc Duc <caothu91@gmail.com>
2017-08-17 12:32:50 +0700

    js | experiences ajax

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -4,6 +4,7 @@ requirejs.config({
         jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
         jqueryUi: 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min',
         jqueryMigrate: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.min',
+        knockout: 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
         themeJavascript: 'javascript',
     },
     shim: {
--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -8,6 +8,34 @@
 }(function themeFactory($) {
     "use strict";
 
+    var app = {};
+
+    require([
+        'knockout'
+    ], function(ko) {
+
+        var Experiences = function() {
+            this.getExperiences = ko.computed(function() {
+                return app.experiences;
+            }, this);
+        };
+        ko.applyBindings(new Experiences(), document.getElementById('experiences'));
+    });
+
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/experiences.json',
+        cache: false,
+        dataType: 'text',
+        success: function(experiences) {
+            experiences = JSON.parse(experiences);
+            if ($.isEmptyObject(experiences)) {
+                return;
+            }
+            app.experiences = experiences;
+        }
+    });
+
     $(function() {
         $(document.body).show();
     });
