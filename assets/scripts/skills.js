44b48445dae12af0e2e7799ace373257dff1fe23
Tran Ngoc Duc <caothu91@gmail.com>
2017-08-17 18:32:20 +0700

    js | skills : bug load items

--- assets/scripts/skills.js
+++ assets/scripts/skills.js
@@ -7,49 +7,38 @@ define([
     var Skills = function() {
         var self = this;
 
-        this._skills = [];
-
-        this.skillsLoad = function() {
+        this._skillsLoad = function() {
             $.ajax({
                 method: 'GET',
                 url: 'assets/data/skills.json',
                 cache: false,
                 dataType: 'text',
-                success: function(skills) {
-                    skills = JSON.parse(skills);
-                    if ($.isEmptyObject(skills)) {
-                        return;
-                    }
-                    $.each(skills, function(index, skills) {
-                        self._skills.push({
-                            skill: index,
+                success: function(resp) {
+                    $.each(JSON.parse(resp), function(type, skills) {
+                        self.$skills.push({
+                            skill: type,
                             type: true,
                             start: 0
                         });
                         $.each(skills, function(index, skill) {
-                            self._skills.push({
+                            self.$skills.push({
                                 skill: skill,
                                 type: false,
                                 start: 0
                             });
                         });
                     });
+
                 }
             });
         };
 
-        this.getSkills = function() {
-            if ($.isEmptyObject(self._skills)) {
-                self.skillsLoad();
-            }
-            return self._skills;
+        this._skills = function() {
+            self._skillsLoad();
+            return [];
         };
 
-        this.skills = ko.computed(function() {
-            return this.getSkills;
-        }, this);
-
-        this.isLoaded = ko.observable('hehe');
+        this.$skills = ko.observableArray(this._skills());
     };
 
     return {
