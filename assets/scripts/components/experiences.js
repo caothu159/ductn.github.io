a6c2b88630341c0771bf1212b21cb1487fa33dc2
Tran Ngoc Duc <caothu91@gmail.com>
2017-08-24 09:06:04 +0700

    js | re-order desc by time

--- assets/scripts/components/experiences.js
+++ assets/scripts/components/experiences.js
@@ -11,6 +11,9 @@ define([
         this._experiencesLoad = function() {
             $.getJSON('assets/data/experiences.json', function(resp) {
                 self.$experiences(resp);
+                self.$experiences.sort(function(left, right) {
+                    return self._getDateFromString(left.time) > self._getDateFromString(right.time) ? -1 : 1;
+                });
             });
         };
 
@@ -20,6 +23,19 @@ define([
         };
 
         this.$experiences = ko.observableArray(this._experiences());
+
+        this._getDateFromString = function _getDateFromString(str) {
+            str = str.split('/');
+            var date = new Date('1970/1/1');
+            if (str.length) {
+                date.setFullYear(str.pop());
+            }
+            if (str.length) {
+                date.setMonth(str.pop());
+            }
+            // date.toLocaleDateString('en-US', { year: 'numeric', month: 'long'})
+            return date;
+        };
     };
 
     return {
